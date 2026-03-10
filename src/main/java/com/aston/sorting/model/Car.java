b641ed6c1063631a40c9d04d1cb86eb0bf8f11fb
al3xStark <starkov.al3xandr@gmail.com>
2026-03-11 02:46:29 +0300

    Implement Car model with Builder pattern

--- src/main/java/com/aston/sorting/model/Car.java
+++ src/main/java/com/aston/sorting/model/Car.java
@@ -1,8 +1,73 @@
 package com.aston.sorting.model;
 
+import java.util.Objects;
 
 public class Car {
 
-    //TODO: Реализовать модель и Builder
+    private final int horsePower;
+    private final String model;
+    private final int year;
 
+    private Car(Builder builder) {
+        this.horsePower = builder.horsePower;
+        this.model = builder.model;
+        this.year = builder.year;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{horsePower=" + horsePower + ", model='" + model + "', year=" + year + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return horsePower == car.horsePower
+                && year == car.year
+                && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, model, year);
+    }
+
+    public static class Builder {
+
+        private int horsePower;
+        private String model;
+        private int year;
+
+        public Builder horsePower(int horsePower) {
+            this.horsePower = horsePower;
+            return this;
+        }
+
+        public Builder model(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public Builder year(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
 }
