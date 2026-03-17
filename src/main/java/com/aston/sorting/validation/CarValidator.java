378736dc788bbe1301a1824ea3ce877f976aa54a
Inessa Babenko <iness.tt.eln@gmail.com>
2026-03-17 20:59:14 +0700

    fix: tests for invalid year before 1886

--- src/main/java/com/aston/sorting/validation/CarValidator.java
+++ src/main/java/com/aston/sorting/validation/CarValidator.java
@@ -1,6 +1,7 @@
 package com.aston.sorting.validation;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
 
 public class CarValidator {
@@ -9,19 +10,16 @@ public class CarValidator {
     }
 
     public static boolean validateHorsePower(int horsePower) {
-        // TODO: реализовать валидацию (например, > 0)
         return 0 < horsePower;
     }
 
     public static boolean validateModel(String model) {
-        // TODO: реализовать валидацию (не пустая строка)
         if (model == null) return false;
         return  ! model.isBlank();
     }
 
     public static boolean validateYear(int year) {
-        // TODO: реализовать валидацию (например, >= 1886 и <= текущий год)
-        int currentYear = Instant.now().atZone(ZoneId.systemDefault()).getYear();
-        return 1886 < year && year <= currentYear;
+        int currentYear = LocalDate.now().getYear();
+        return 1886 <= year && year <= currentYear;
     }
 }
