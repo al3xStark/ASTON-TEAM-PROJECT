6ee272ee244c7016c8a6fb0c7fa12e5c904b76a1
Gera <gerain15@yandex.ru>
2026-03-21 19:32:41 +0700

    gitcommit-m"[feat] реализован RandomInputHandler"

--- src/main/java/com/aston/sorting/io/input/RandomInputHandler.java
+++ src/main/java/com/aston/sorting/io/input/RandomInputHandler.java
@@ -9,6 +9,19 @@ public class RandomInputHandler implements InputHandler {
     @Override
     public List<Car> read(int size) {
         // TODO: использовать Stream.generate() для создания случайных Car
+        String[] models = {"BMW", "Audi", "Mercedes", "Toyota", "Honda", "Maserati", "Hyundai", "Ferrari", "Kia", "Subaru"};
+        int minYear = 1886;
+        int maxYear = java.time.Year.now().getValue();
+        List<Car> cars = new ArrayList<>();
+        Random rand = new Random();
+
+        return Stream.generate(() -> new Car.Builder()
+                        .model(models[rand.nextInt(models.length)])
+                        .horsePower(rand.nextInt(301) + 100) // 100-400 л.с.
+                        .year(rand.nextInt(maxYear - minYear + 1) + minYear)
+                        .build())
+                .limit(size)
+                .collect(Collectors.toList());
         return null;
     }
 }
