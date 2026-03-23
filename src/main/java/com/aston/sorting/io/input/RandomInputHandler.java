d53ca2256290bfa0df98efeafcfd98dc4c9257fc
Gera <gerain15@yandex.ru>
2026-03-23 20:12:47 +0700

    [feat] реализация randominputhandler

--- src/main/java/com/aston/sorting/io/input/RandomInputHandler.java
+++ src/main/java/com/aston/sorting/io/input/RandomInputHandler.java
@@ -1,31 +1,30 @@
 package com.aston.sorting.io.input;
-
 import com.aston.sorting.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RandomInputHandler implements com.aston.sorting.io.input.InputHandler {
+    private static final String[] MODELS = {
+            "BMW", "Audi", "Mercedes", "Toyota", "Honda",
+            "Maserati", "Hyundai", "Ferrari", "Kia", "Subaru"
+    };
+    private static final int MIN_YEAR = 1886;
+    private static final int MAX_YEAR = java.time.Year.now().getValue();
+    private static final Random rand = new Random();
+
 
     @Override
     public List<Car> read(int size) {
 
-            String[] models = {"BMW", "Audi", "Mercedes", "Toyota", "Honda", "Maserati", "Hyundai", "Ferrari", "Kia", "Subaru"};
-            int minYear = 1886;
-            int maxYear = java.time.Year.now().getValue();
-            List<Car> cars = new ArrayList<>();
-            Random rand = new Random();
-
             return Stream.generate(() -> new Car.Builder()
-                            .model(models[rand.nextInt(models.length)])
+                            .model(MODELS[rand.nextInt(MODELS.length)])
                             .horsePower(rand.nextInt(301) + 100) // 100-400 л.с.
-                            .year(rand.nextInt(maxYear - minYear + 1) + minYear)
+                            .year(rand.nextInt(MAX_YEAR - MIN_YEAR + 1) + MIN_YEAR)
                             .build())
-                    .limit(size)
-                    .collect(Collectors.toList());
+                            .limit(size)
+                            .toList();
 
     }
 }
