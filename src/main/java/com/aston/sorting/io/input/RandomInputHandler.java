4a013ed5a1c8246b0b7926b26a85a4e5c9a6f103
Gera <gerain15@yandex.ru>
2026-03-23 19:43:59 +0700

    [feat] реализация randominputhandler

--- src/main/java/com/aston/sorting/io/input/RandomInputHandler.java
+++ src/main/java/com/aston/sorting/io/input/RandomInputHandler.java
@@ -2,13 +2,30 @@ package com.aston.sorting.io.input;
 
 import com.aston.sorting.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class RandomInputHandler implements InputHandler {
+public class RandomInputHandler implements com.aston.sorting.io.input.InputHandler {
 
     @Override
     public List<Car> read(int size) {
-        // TODO: использовать Stream.generate() для создания случайных Car
-        return null;
+
+            String[] models = {"BMW", "Audi", "Mercedes", "Toyota", "Honda", "Maserati", "Hyundai", "Ferrari", "Kia", "Subaru"};
+            int minYear = 1886;
+            int maxYear = java.time.Year.now().getValue();
+            List<Car> cars = new ArrayList<>();
+            Random rand = new Random();
+
+            return Stream.generate(() -> new Car.Builder()
+                            .model(models[rand.nextInt(models.length)])
+                            .horsePower(rand.nextInt(301) + 100) // 100-400 л.с.
+                            .year(rand.nextInt(maxYear - minYear + 1) + minYear)
+                            .build())
+                    .limit(size)
+                    .collect(Collectors.toList());
+
     }
 }
